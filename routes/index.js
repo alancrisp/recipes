dca3c3f11609dbd6aa1271fb347380d77c66504d
Alan Crisp <alan.crisp@gmail.com>
2023-05-21 17:13:22 +0100

    Refactor callbacks

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,23 @@ var express = require('express');
 var router = express.Router();
 var recipeRepo = require('../src/repo/recipe.repo.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  recipeRepo.getRecipes((err, rows, fields) => {
-    res.render('index', { title: 'Recipes', recipes: rows });
+router.get('/', (req, res, next) => {
+  recipeRepo.getRecipes().then((recipes) => {
+    res.render('index', { title: 'Recipes', recipes: recipes });
   });
 });
 
 /* TODO rename router if recipe routes stay here */
 router.get('/recipe/:recipeId', (req, res, next) => {
-    recipeRepo.getRecipe(req.params.recipeId, (err, rows, fields) => {
-      res.render('recipe', { recipe: rows[0] });
-    });
+  recipeRepo.getRecipe(req.params.recipeId).then((recipe) => {
+    res.render('recipe', { recipe: recipe });
+  });
 });
 
 router.post('/recipe/add', (req, res, next) => {
-    recipeRepo.createRecipe(req.body.name, (err, results, fields) => {
-        res.redirect('/recipe/' + results.insertId);
-    });
+  recipeRepo.createRecipe(req.body.name).then((recipeId) => {
+    res.redirect('/recipe/' + recipeId);
+  });
 });
 
 module.exports = router;
--- src/repo/recipe.repo.js
+++ src/repo/recipe.repo.js
@@ -1,19 +1,34 @@
 const db = require('../db.js');
 
-const getRecipes = (callback) => {
-  db.query('SELECT * FROM recipe', callback);
+const getRecipes = () => {
+  return new Promise((resolve, reject) => {
+    db.query('SELECT * FROM recipe', (error, result) => {
+      if (error) throw error;
+      resolve(result);
+    });
+  });
 }
 
-const getRecipe = (recipeId, callback) => {
-    db.query('SELECT * FROM recipe WHERE recipeId=?', [recipeId], callback);
+const getRecipe = (recipeId) => {
+  return new Promise((resolve, reject) => {
+    db.query('SELECT * FROM recipe WHERE recipeId=?', [recipeId], (error, result) => {
+      if (error) throw error;
+      resolve(result[0]);
+    });
+  });
 }
 
-const createRecipe = (name, callback) => {
-    db.query('INSERT INTO recipe (name) VALUES (?)', [name], callback);
+const createRecipe = (name) => {
+  return new Promise((resolve, reject) => {
+    db.query('INSERT INTO recipe (name) VALUES (?)', [name], (error, result) => {
+      if (error) throw error;
+      resolve(result.insertId);
+    });
+  });
 }
 
 module.exports = {
-    getRecipes,
-    getRecipe,
-    createRecipe,
+  getRecipes,
+  getRecipe,
+  createRecipe,
 }
