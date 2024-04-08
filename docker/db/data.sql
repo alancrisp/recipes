fa181bc1fe2db172ffca51bf22aadf28f369035c
Alan Crisp <alan.crisp@ask4.com>
2024-04-08 22:29:08 +0100

    Add ingredient and unit tables

---
+++ docker/db/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO unit (name, abbrev)
+VALUES
+('Grams', 'g'),
+('Kilograms', 'kg'),
+('Milliliters', 'ml');
--- docker/db/schema.sql
+++ docker/db/schema.sql
@@ -3,3 +3,21 @@ CREATE TABLE recipe (
     name varchar(100) NOT NULL,
     PRIMARY KEY (recipeId)
 );
+
+CREATE TABLE unit (
+    unitId int unsigned NOT NULL AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    abbrev varchar(100) NOT NULL,
+    PRIMARY KEY (unitId)
+);
+
+CREATE TABLE ingredient (
+    ingredientId int unsigned NOT NULL AUTO_INCREMENT,
+    recipeId int unsigned NOT NULL,
+    name varchar(100) NOT NULL,
+    qty int unsigned NOT NULL,
+    unitId int unsigned,
+    PRIMARY KEY (ingredientId),
+    FOREIGN KEY (recipeId) REFERENCES recipe (recipeId),
+    FOREIGN KEY (unitId) REFERENCES unit (unitId)
+);
